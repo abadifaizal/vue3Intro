73c144eb926dd6832daab8aa645591f308c8f669
abadifaizal <abadifaizal@gmail.com>
2023-10-09 15:42:29 +0900

    vue router lesson

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,51 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import EventList from '@/views/EventList.vue'
+import EventLayout from '@/views/event/Layout.vue'
+import EventDetails from '@/views/event/Details.vue'
+import EventRegister from '@/views/event/Register.vue'
+import EventEdit from '@/views/event/Edit.vue'
+import AboutView from '@/views/About.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'event-list',
+      component: EventList,
+      props: (route) => ({
+        page: parseInt(route.query.page) || 1,
+      }),
+    },
+    {
+      path: '/event/:id',
+      name: 'event-layout',
+      props: true,
+      component: EventLayout,
+      children: [
+        {
+          path: '',
+          name: 'event-details',
+          component: EventDetails,
+        },
+        {
+          path: 'register',
+          name: 'event-register',
+          component: EventRegister,
+        },
+        {
+          path: 'edit',
+          name: 'event-edit',
+          component: EventEdit,
+        },
+      ],
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+      component: AboutView,
+    },
+  ],
 })
 
 export default router
